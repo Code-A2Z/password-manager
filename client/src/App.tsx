d257480851f5af2a487c0a59a0a211efdf469782
archa8 <archa.ug22.me@nitp.ac.in>
2025-07-13 23:43:16 +0530

    Fix: updated and cleaned App.tsx logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,7 @@
-import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
-import SignIn from './components/SignIn'
-import { Box, Typography } from '@mui/material'
+import UserAuth from './pages/UserAuth'
 
 function App() {
-  return (
-    <>
-      <SignedOut>
-        <SignIn />
-      </SignedOut>
-
-      <SignedIn>
-        <Box p={3}>
-          <Box display="flex" justifyContent="flex-end">
-            <UserButton />
-          </Box>
-          <Typography variant="h5">
-            You’re logged in. Redirecting...
-          </Typography>
-        </Box>
-      </SignedIn>
-    </>
-  )
+  return <UserAuth />
 }
 
-export default App;
+export default App
