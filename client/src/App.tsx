1723884fb9c942a20dab37cee1dae0bc53fac43e
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-07-16 02:00:13 +0530

    update login code flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,12 @@
 import { Box } from '@mui/material';
-import { SignedIn, SignedOut } from '@clerk/clerk-react';
-import SignIn from './components/SignIn';
 import Navbar from './components/Navbar';
 
 function App() {
   // TODO: Use `isLoaded` - Handle loading state to avoid flickering and show a loading spinner or message
   return (
-    <>
-      {/* If user session is not created or deleted */}
-      <SignedOut>
-        <SignIn />
-      </SignedOut>
-
-      {/* If user session is created */}
-      <SignedIn>
-        <Box>
-          <Navbar />
-        </Box>
-      </SignedIn>
-    </>
+    <Box>
+      <Navbar />
+    </Box>
   );
 };
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,11 @@ import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ClerkProvider } from '@clerk/clerk-react';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-react';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import App from './App.tsx';
+import SignIn from './components/SignIn.tsx';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -18,7 +19,17 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <ThemeProvider theme={theme}>
-          <App />
+          <>
+            {/* If user session is not created or deleted */}
+            <SignedOut>
+              <SignIn />
+            </SignedOut>
+
+            {/* If user session is created */}
+            <SignedIn>
+              <App />
+            </SignedIn>
+          </>
         </ThemeProvider>
       </ClerkProvider>
     </BrowserRouter>
