9dc94f8a91f1cf3d7636a6805b9318d956adcb40
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-07-16 01:51:34 +0530

    lint changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ClerkProvider } from '@clerk/clerk-react'
-import './index.css'
-import App from './App.tsx'
-import { ThemeProvider } from '@mui/material/styles'
-import theme from './theme'
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import App from './App.tsx';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error('Missing Publishable Key');
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </ClerkProvider>
-  </React.StrictMode>
-)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </ClerkProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
