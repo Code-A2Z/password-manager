ce7257fc732694bea6fea5f8d3e37a5f3b1c401a
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-07-17 01:07:45 +0530

    Route setup & Lint changes (#10)
    * setup clerk & userauth
    
    * lint changes
    
    * add env file
    
    * setup navbar
    
    * update login code flow
    
    * renamed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
-import UserAuth from './pages/UserAuth'
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
 
-function App() {
-  return <UserAuth />
-}
+const App = () => {
+  // TODO: Use `isLoaded` - Handle loading state to avoid flickering and show a loading spinner or message
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ClerkProvider } from '@clerk/clerk-react'
-import './index.css'
-import App from './App.tsx'
-import { ThemeProvider } from '@mui/material/styles'
-import theme from './theme'
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import App from './App.tsx';
+import UserAuth from './pages/UserAuth.tsx';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error('Missing Publishable Key');
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </ClerkProvider>
-  </React.StrictMode>
-)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <ThemeProvider theme={theme}>
+          <>
+            {/* If user session is not created or deleted */}
+            <SignedOut>
+              <UserAuth />
+            </SignedOut>
+
+            {/* If user session is created */}
+            <SignedIn>
+              <App />
+            </SignedIn>
+          </>
+        </ThemeProvider>
+      </ClerkProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
