c13ac35b42465acb0a4382ba47eee38bf6bfa94c
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-07-19 17:13:34 +0530

    setup navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
 import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Folders from './pages/Folders';
 
 const App = () => {
   // TODO: Use `isLoaded` - Handle loading state to avoid flickering and show a loading spinner or message
   return (
-    <Box>
+    <>
       <Navbar />
-    </Box>
+      <Box
+        sx={{
+          maxWidth: '85rem',
+          margin: '0 auto',
+          padding: '1rem',
+        }}
+      >
+        <Routes>
+          <Route path='/' element={<Outlet />}>
+            <Route index element={<Folders />} />
+          </Route>
+        </Routes>
+      </Box>
+    </>
   );
 };
 
