60a2375a596669669730f7f56ac4cdfc990bf9f3
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-07-21 09:01:10 +0530

    Setup folders page (#11)
    * setup navbar
    
    * implement basic folders page
    
    * adding styling & fake data
    
    * fixes responsiveness
    
    * implement footer
    
    * write 'component:nav'
    
    * update changes from copilot
    
    * fixes nav & footer responsiveness

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,38 @@
 import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Folders from './pages/Folders';
+import Footer from './components/Footer';
 
 const App = () => {
   // TODO: Use `isLoaded` - Handle loading state to avoid flickering and show a loading spinner or message
   return (
-    <Box>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
       <Navbar />
+
+      <Box
+        component="main"
+        sx={{
+          flex: 1,
+          maxWidth: '85rem',
+          width: '100%',
+          margin: '0 auto',
+        }}
+      >
+        <Routes>
+          <Route path='/' element={<Outlet />}>
+            <Route index element={<Folders />} />
+          </Route>
+        </Routes>
+      </Box>
+
+      <Footer />
     </Box>
   );
 };
